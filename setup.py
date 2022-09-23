1fdc413adc10236f99877e8338e87e9609699780
Aleksandar <aleksandar.kartelj@gmail.com>
2022-09-23 18:15:40 +0200

    Setup.py adapted.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='rils',
+setup(name='rils-rols',
     version='0.0',
-    description='Symbolic Regression Tool Based On The Iterated Local Search',
+    description='RILS-ROLS: Robust Symbolic Regression via Iterated Local Search and Ordinary Least Squares',
     author='Aleksandar Kartelj, Marko Đjukanović',
     author_email='aleksandar.kartelj@gmail.com',
-    url='https://github.com/kartelj/rils',
+    url='https://github.com/kartelj/rils-rols',
     packages = ['rils-rols'],
-    package_dir = {'rils':'rils_rols'}, 
+    package_dir = {'rils-rols':'rils_rols'}, 
     install_requires=['joblib', "sklearn", "sympy", "chardet", "certifi", "idna"],
 )
