36982ce4f26d18fcb4a256f3976e2b00210750aa
janoPig <jan.pigos@gmail.com>
2023-08-28 20:02:19 +0200

    Enable profiling in test1.py

--- test1.py
+++ test1.py
@@ -3,6 +3,10 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from rils_rols.utils import R2, RMSE
 from rils_rols.rils_rols import RILSROLSRegressor
+import sys
+import cProfile
+
+PROFILE = True
 
 dataset = 'feynman_I_15_3x'
 pmlb_cache = "../pmlb/datasets"
@@ -11,10 +15,10 @@ label="target"
 test_size = 0.25
 seed = 12345
 max_fit_calls = 10000
-max_seconds = 10000
+max_seconds = 100 if PROFILE else 10000
 
 input_data = fetch_data(dataset,  local_cache_dir=pmlb_cache)
-     
+
 feature_names = [x for x in input_data.columns.values if x != label]
 feature_names = np.array(feature_names)
 
@@ -26,8 +30,18 @@ assert(X.shape[1] == feature_names.shape[0])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=seed)
 
 rils = RILSROLSRegressor(max_fit_calls=max_fit_calls, max_seconds=max_seconds, random_state = seed, verbose=True)
-        
-rils.fit(X_train, y_train)
+
+if PROFILE:
+    with cProfile.Profile() as pr:
+        rils.fit(X_train, y_train)
+    with open('profile_tottime.txt', 'w') as sys.stdout:
+        pr.print_stats(1)
+    with open('profile_cumtime.txt', 'w') as sys.stdout:
+        pr.print_stats(2)
+    sys.stdout = sys.__stdout__
+    sys.stderr = sys.__stderr__
+else:
+    rils.fit(X_train, y_train)
 report_string = rils.fit_report_string(X_train, y_train)
 rils_R2 = ""
 rils_RMSE = ""
