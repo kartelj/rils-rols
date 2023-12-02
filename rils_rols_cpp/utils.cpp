1dc1c9e20599090b65d2f7e8f9bd61b47ec1a20f
Jan Pigos <jan.pigos@gmail.com>
2023-12-02 10:15:26 +0100

    Use eigen to compute R2 and RMSE

--- rils_rols_cpp/utils.cpp
+++ rils_rols_cpp/utils.cpp
@@ -2,23 +2,14 @@
 #include "eigen/Eigen/Dense"
 
 double utils::R2(const Eigen::ArrayXd &y, const Eigen::ArrayXd &yp) {
-	double y_avg = 0;
-	for (auto yi : y)
-		y_avg += yi;
-	y_avg /= y.size();
-	double ssr = 0, sst = 0;
-	for (int i = 0; i < y.size(); i++) {
-		ssr += pow(y[i] - yp[i], 2);
-		sst += pow(y[i] - y_avg, 2);
-	}
+	const auto y_avg = y.mean();
+	const auto ssr = ((y - yp) * (y - yp)).sum();
+	const auto sst = ((y - y_avg) * (y - y_avg)).sum();
 	return 1 - ssr / sst;
 }
 
 double utils::RMSE(const Eigen::ArrayXd& y, const Eigen::ArrayXd& yp) {
-	double rmse = 0;
-	for (int i = 0; i < y.size(); i++)
-		rmse += pow(y[i] - yp[i], 2);
-	return sqrt(rmse / y.size());
+	return sqrt(((y - yp) * (y - yp)).mean());
 }
 
 double utils::classification_accuracy(const Eigen::ArrayXd& y, const Eigen::ArrayXd& yp) {
